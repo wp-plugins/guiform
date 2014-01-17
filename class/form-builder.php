bb085da81e05872c88868a6408a088c997c19e45
russellpabon <russellpabon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-17 19:30:26 +0000

    Clean repository
    git-svn-id: https://plugins.svn.wordpress.org/guiform/trunk@840463 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class/form-builder.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Form_builder{
-	public function display(){
-		global $guif;
-		$html = "<div id='GuiForm' class='container form-builder clearfix'></div>";
-		return "\n\n\n$html\n\n\n";
-	}
-}
-?>
