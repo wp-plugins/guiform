38fd894396c1193d4dc002ccc42a0ae68cb33037
russellpabon <russellpabon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-17 02:03:41 +0000

    Add plugin files in trunk
    git-svn-id: https://plugins.svn.wordpress.org/guiform/trunk@840082 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ class/form-builder.php
@@ -0,0 +1,9 @@
+<?php
+class Form_builder{
+	public function display(){
+		global $guif;
+		$html = "<div id='GuiForm' class='container form-builder clearfix'></div>";
+		return "\n\n\n$html\n\n\n";
+	}
+}
+?>
---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+  * Plugin version number.
+	*
+	* @since 1.0
+	*/
+define('GUIFORM_VERSION', '1.0');
+
+/**
+	* Setup global table prefix names.
+	*
+	* @since 1.0
+	*/
+global $wpdb;
+$wpdb->guiform         = $wpdb->prefix.'guiform';
+$wpdb->guiform_options = $wpdb->prefix.'guiform_options';
+$wpdb->guiform_form    = $wpdb->prefix.'guiform_form_';
