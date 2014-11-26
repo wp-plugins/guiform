14a929654abb1ebd737eb84fad71234c4e7ef9a9
russellpabon <russellpabon@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-26 00:32:37 +0000

    import new files
    git-svn-id: https://plugins.svn.wordpress.org/guiform/trunk@1032830 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ functions-sample.php
@@ -0,0 +1,20 @@
+<?php
+
+//Rename this file to "functions.php"
+
+class Custom_Class extends GuiForm_Module{
+	
+	function __construct() {
+		//$this->_addAction('guiform_filter_{Form ID}_save_entry', array($this, 'saveEntry'), 10, 2);
+  }
+  
+  public function saveEntry($args, $props){
+  	//Do whatever you want!
+  }
+  
+}
+
+//Check if custom class is already loaded.
+if ( ! class_exists( 'Custom_Class', false ) ) {
+   new Custom_Class();
+}
